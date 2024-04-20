8ed7dfbfd1786789c8d40d83ef1bbb550fd549ea
carlostuuling <71810387+carlostuuling@users.noreply.github.com>
2024-04-20 19:52:06 +0300

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pedo Archons chamber",
+  title: "Hajime SPIN",
   description: "HI JOSDH ",
 };
 
