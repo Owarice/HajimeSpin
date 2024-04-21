6fefb399514be77b323bf55100e31f7aa941106d
Owarice <Carlos.tuuling22@gmail.com>
2024-04-21 13:31:42 +0300

    music

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
 'use client'
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const audio = new Audio('/music.mp3');
-    audio.loop = true;
-    audio.play();
+  const [audioStarted, setAudioStarted] = useState(false);
 
+  useEffect(() => {
+    if (audioStarted) {
+      const audio = new Audio('/music.mp3');
+      audio.loop = true;
+      audio.play();
+      return () => {
+        audio.pause();
+        audio.currentTime = 0;
+      };
+    }
+  }, [audioStarted]);
+
+  const startAudio = () => {
+    setAudioStarted(true);
+    document.removeEventListener('click', startAudio);
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', startAudio);
     return () => {
-      audio.pause();
-      audio.currentTime = 0;
+      document.removeEventListener('click', startAudio);
     };
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#D4B8FF]">
-      <a href="https://youtu.be/R2yOpkIGTeg?si=lPIUnzwogOFfXvWP">
         <img className="animate-spin" src="/next.png" alt=""/>
-      </a>
     </main>
   );
 }
